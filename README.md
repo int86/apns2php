8ec919fccd14d77946d30458d146a4468dfbcb73
bjdgyc <bjdgyc@163.com>
2016-11-01 12:49:18 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # apns2php
-apns2php implementation of the HTTP/2 Protocol by PHPCURL
+
+## Introduction
+apns2php implemente the HTTP/2 Protocol by PHP curl_multi
+
+## Notice
+* PHP>=5.5.24 提供对HTTP/2的支持
+* libcurl需要 >=7.38.0
+* openssl >=1.0.2
+* Nghttp2
+
+
+## Example
+
+``` php
+
+    $push = new Apns(Apns::ENVIRONMENT_PRODUCTION);
+    $push->setCertFile($pem);
+    $push->setTopic($topic);
+    
+    $playload = array(
+        'aps' => array(
+            'alert' => 'alert',
+            "badge" => 1,
+            "sound" => "default",
+        ),
+    );
+    $push->setPayload($playload);
+    $push->setThreads(2);
+    $push->connect();
+    $datas[] = [
+        'token' => 'token',//apns token
+        'id' => 'id', // 自定义id
+    ];    
+    $result = $push->send($datas);
+    var_dump($result);
+    
+```
+
+
+## Test
+
+* 单机8核16g阿里云vps
+* CentOS release 6.5 (Final) (2.6.32-573.18.1.el6.x86_64)
+* PHP=7.0.12
+* OpenSSL/1.1.0b [下载](https://www.openssl.org/source/openssl-1.1.0b.tar.gz)
+* libcurl/7.50.3 [下载](https://curl.haxx.se/download/curl-7.50.3.tar.gz)
+* nghttp2/1.16.0 [下载](https://github.com/nghttp2/nghttp2/releases/download/v1.16.0/nghttp2-1.16.0.tar.gz)
+* 开启线程数 `40`
+
+线上实测，每秒推送`50000`条
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ apns.php
@@ -0,0 +1,242 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bjd
+ * Date: 2016/10/27
+ * Time: 17:36
+ */
+class Apns
+{
+
+    const ENVIRONMENT_SANDBOX = 0;
+    const ENVIRONMENT_PRODUCTION = 1;
+
+    protected $_nEnvironment;
+    protected $_aHTTPServiceURLs = array(
+        'https://api.development.push.apple.com:443', // Sandbox environment
+        'https://api.push.apple.com:443', // Production environment
+    );
+
+    protected $_aHTTPErrorResponseMessages = array(
+        200 => 'Success',
+        400 => 'Bad request',
+        403 => 'There was an error with the certificate',
+        405 => 'The request used a bad :method value. Only POST requests are supported',
+        410 => 'The device token is no longer active for the topic',
+        413 => 'The notification payload was too large',
+        429 => 'The server received too many requests for the same device token',
+        500 => 'Internal server error',
+        503 => 'The server is shutting down and unavailable',
+        0 => 'Internal error',
+    );
+
+
+    protected $_sProviderCertificateFile;
+    protected $_spassword;
+    private $_topic;
+    private $_payload;
+
+    private $_mh = null;
+    private $_chs = array();
+    private $_threads = 2;
+
+
+    public function __construct($nEnvironment)
+    {
+        $this->_nEnvironment = $this->_aHTTPServiceURLs[$nEnvironment];
+    }
+
+    public function setCertFile($file, $spassword = '')
+    {
+        $this->_sProviderCertificateFile = $file;
+        $this->_spassword = $spassword;
+        return $this;
+    }
+
+    public function setTopic($topic)
+    {
+        $this->_topic = $topic;
+        return $this;
+    }
+
+    public function setPayload($payload)
+    {
+        $this->_payload = json_encode($payload);
+        return $this;
+    }
+
+    public function setThreads($threads)
+    {
+        $this->_threads = $threads;
+    }
+
+    public function connect()
+    {
+        if (empty($this->_nEnvironment) || empty($this->_sProviderCertificateFile)
+            || empty($this->_topic) || empty($this->_process)
+        ) {
+            die("init check error");
+        }
+
+        $this->_mh = curl_multi_init();//创建多个curl语柄
+
+        if (!$this->_mh) {
+            die("curl multi init error");
+        }
+
+        for ($i = 0; $i < $this->_threads; $i++) {
+            $this->_initCurl($i);//创建多个curl语柄
+        }
+    }
+
+    public function close()
+    {
+        foreach ($this->_chs as $ch) {
+            curl_close($ch);
+            curl_multi_remove_handle($this->_mh, $ch);   //释放资源
+        }
+        $this->_chs = [];
+        curl_multi_close($this->_mh);
+    }
+
+    public function send($datas)
+    {
+        $startime = getmicrotime();
+        $reslut = [];
+        $i = 0;
+        foreach ($datas as $data) {
+            $i++;
+            $res[] = $data;
+            if ($i % $this->_threads == 0) {
+                $reslut = array_merge($reslut, $this->_muitSend($res));
+                $res = [];
+            }
+        }
+
+        if (!empty($res)) {
+            $this->close();
+            $this->connect();
+            $reslut = array_merge($reslut, $this->_muitSend($res));
+        }
+
+        $diff_time = getmicrotime() - $startime;
+
+        return array(
+            'diff_time' => $diff_time,
+            'reslut' => $reslut,
+        );
+    }
+
+    private function _muitSend($datas)
+    {
+        // $data  => token,id
+        $datas = array_values($datas);
+        foreach ($datas as $k => $data) {
+            $this->_updateCurl($k, $data);
+        }
+
+        $running = null;
+        // 执行批处理句柄
+        do {
+//            usleep(1000);
+            curl_multi_exec($this->_mh, $running);
+            curl_multi_select($this->_mh);
+        } while ($running > 0);
+
+        $reslut = array();
+        foreach ($datas as $k => $data) {
+            $res = array();
+            $res['code'] = 0;
+            $res['id'] = $data['id'];
+            $res['token'] = $data['token'];
+
+            $err = curl_error($this->_chs[$k]);
+            if (!empty($err)) {
+                $res['err'] = $err;
+                $reslut[] = $res;
+                continue;
+            }
+
+            $res['code'] = curl_getinfo($this->_chs[$k], CURLINFO_HTTP_CODE);//返回头信息
+            $res['reason'] = $this->_aHTTPErrorResponseMessages[$res['code']];
+            if ($res['code'] == 200) {
+                $reslut[] = $res;
+                continue;
+            }
+
+            $res['content'] = curl_multi_getcontent($this->_chs[$k]);//获得返回信息
+
+            $reslut[] = $res;
+        }
+
+        return $reslut;
+    }
+
+    function _initCurl($k)
+    {
+        if (!empty($this->_chs[$k])) {
+            return;
+        }
+
+        $this->_chs[$k] = curl_init();
+        if (!$this->_chs[$k]) {
+            die("curl $k init error");
+        }
+
+        $setopt = curl_setopt_array($this->_chs[$k],
+            array(
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+                CURLOPT_SSLCERT => $this->_sProviderCertificateFile,
+                CURLOPT_SSLCERTPASSWD => empty($this->_spassword) ? null : $this->_spassword,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_USERAGENT => 'ApnsPHP',
+                CURLOPT_CONNECTTIMEOUT => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_SSL_VERIFYPEER => true,
+                CURLOPT_VERBOSE => false,
+                CURLOPT_MAXREDIRS => 7,
+                CURLOPT_FOLLOWLOCATION => 1,
+                CURLOPT_POST => true,
+            )
+        );
+        if (!$setopt) {
+            die("curl $k setopt error");
+        }
+
+        $res = curl_multi_add_handle($this->_mh, $this->_chs[$k]);
+        if ($res != 0) {
+            die("curl $k add_handle error($res)");
+        }
+
+
+    }
+
+    private function _updateCurl($k, $data)
+    {
+        $headers = array(
+            'Content-Type: application/json',
+            'apns-topic: ' . $this->_topic,
+        );
+        $setopt = curl_setopt_array($this->_chs[$k],
+            array(
+                CURLOPT_URL => sprintf('%s/3/device/%s', $this->_nEnvironment, $data['token']),
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_POSTFIELDS => $this->_payload,
+            )
+        );
+        if (!$setopt) {
+            die("curl $k setopt_update error");
+        }
+
+    }
+
+}
+
+
+//计算当前时间
+function getmicrotime()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
